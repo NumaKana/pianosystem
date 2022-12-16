01db20b4b3903c47ca905b35dd543aeaee8dcf72
Numakana <numakana6@gmail.com>
2022-12-16 15:03:06 +0900

    log kesita

--- pianosystem.py
+++ pianosystem.py
@@ -2,20 +2,30 @@ import streamlit as st
 import os
 
 st.set_page_config(
-    page_title="アプリケーション名",
+    page_title="ピアノ練習システム",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
-
 num=2
-i=0 
-file = "sheet/file.xml"
+col_img= st.columns(num)
+file = "sheet/file.mxl"
 
 uploaded_files = st.sidebar.file_uploader("upload .mxl file")
-col= st.columns(num)
+
+col_button = st.sidebar.columns(2)
+show_default = col_button[0].button("default")
+show_color = col_button[1].button("4小節ずつ")
+
+col_page = st.sidebar.columns(2)
+page_back = col_page[0].button("＜")
+page_next = col_page[1].button("＞")
 
 def init():
+    if st.button("makesvg"):
+        st.session_state.m = 0
+        makesvg("sheet/file.mxl")
+        addcolor(st.session_state.m)
     if uploaded_files:
         makesvg(file)
         addcolor()
@@ -23,24 +33,38 @@ def init():
         st.session_state.filename = ""
     if 'n' not in st.session_state:
         st.session_state.n = 0
+    if 'm' not in st.session_state:
+        st.session_state.m = 0
     
 
 def makesvg(file): #できない！！
     #make_svg.cmdからコマンドを実行する
     cmd_musicxml2ly = "musicxml2ly.cmd"   # .cmdファイルへのパス
-    command = cmd_musicxml2ly  + file
+    command = cmd_musicxml2ly  + " " + file
     #command += " " + "sheet/file.xml" #ここをupload_fileにしたい
     os.system(command)
     os.system("lilypond.cmd sheet/file.ly")
     
-def addcolor():
+def addcolor(m):
     file_name = "sheet/file.ly"
 
     #ファイルをリストで読み込み
     with open(file_name, encoding="utf-8") as f:
         data = f.readlines()
 
-    data.insert(28, '    \override NoteHead.color = #(x11-color "LimeGreen")\n')
+    i = 0
+    for d in data:
+        if d.endswith("% "+str(m+1)+"\n"):
+            data.insert(i+1, '\override NoteHead.color = #(x11-color "red")\n')
+            data.insert(i+1, '\override Stem.color = #red\n')
+            data.insert(i+1, '\override Beam.color = #red\n')
+        if d.endswith("% "+str(m+5)+"\n"):
+            data.insert(i+1, '\override NoteHead.color = #(x11-color "black")\n')
+            data.insert(i+1, '\override Stem.color = #black\n')
+            data.insert(i+1, '\override Beam.color = #black\n')
+        i+=1
+        
+    
 
     #元のファイルに書き込み
     with open("sheet/alt_file.ly", mode='w', encoding="utf-8") as f:
@@ -51,29 +75,39 @@ def addcolor():
 
 def show(filename, n):
     for i in list(range(0,num,1)):
-        with col[i]:
+        with col_img[i]:
             st.image("sheet/"+filename+"-page"+str(i+n+1)+".png", use_column_width=True)
 
 def main():
     init()
-
-    if st.sidebar.button("makesvg"):
-        makesvg("")
-        
-    if st.sidebar.button("default"):
+    if show_default:
         st.session_state.filename = "file"
         show(st.session_state.filename, st.session_state.n)
-
-    if st.sidebar.button("color"):
+    if show_color:
         st.session_state.filename = "alt_file"
         show(st.session_state.filename, st.session_state.n)
+        
+    if st.session_state.filename == "alt_file":
+        col_color = st.sidebar.columns(2)
+        color_back = col_color[0].button("back")
+        color_next = col_color[1].button("next")
+        if color_next:
+            if st.session_state.m < 38: st.session_state.m +=4
+            addcolor(st.session_state.m)
+            show(st.session_state.filename, st.session_state.n)
+        if color_back:
+            if st.session_state.m > 0: st.session_state.m -=4
+            addcolor(st.session_state.m)
+            show(st.session_state.filename, st.session_state.n)
 
-    if st.button("left"): 
+    if page_next: 
+        if st.session_state.n < 0: st.session_state.n += 1
+        show(st.session_state.filename, st.session_state.n)
+    if page_back:
         if st.session_state.n > 0: st.session_state.n -= 1
         show(st.session_state.filename, st.session_state.n)
-    if st.button("right"):
-        if st.session_state.n < num: st.session_state.n += 1
-        show(st.session_state.filename, st.session_state.n)
+        
+    
 
 
 if __name__ == "__main__":
