838b1072dd8f103f24da57390ae0ffd4a721b8aa
Numakana <numakana6@gmail.com>
2022-12-28 23:47:14 +0900

    start command test

--- cmd/lilypond.cmd
+++ cmd/lilypond.cmd
@@ -4,6 +4,6 @@ echo "lilypond Executed"
 
 chcp 65001
 
-call "cmd/lilypond.exe" --output=sheet -dgui=t --png %1
+start "cmd/lilypond.exe" --output=sheet -dgui=t --png %1
 
 echo "lilypond Completed"
