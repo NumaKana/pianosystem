44a0d20e1592e8ccfc12e3a94eb1cb0617bbfcdd
Numakana <numakana6@gmail.com>
2022-12-28 23:35:06 +0900

    python and .exe test

--- cmd/musicxml2ly.cmd
+++ cmd/musicxml2ly.cmd
@@ -2,6 +2,6 @@
 
 echo "musicxml2ly Executed"
 
-python -3.7-32 ".\Lilypond\usr\bin\musicxml2ly.py" --output=sheet/file %1
+python ".\Lilypond\usr\bin\musicxml2ly.py" --output=sheet/file %1
 
 echo "musicxml2ly Completed"
