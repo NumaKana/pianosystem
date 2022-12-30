71dbb58761bffbce13104a4c063ff7102f17d58d
Numakana <numakana6@gmail.com>
2022-12-31 00:29:48 +0900

    ??

--- cmd/lilypond.sh
+++ cmd/lilypond.sh
@@ -5,6 +5,6 @@ echo "lilypond Executed"
 chmod -R 777 'LilyPond'
 chmod -R 777 'cmd'
 
-"cmd/lilypond.exe" --output=sheet -dgui=t --png $1
+"lilypond-2.22.2-1.mingw (2).exe" --output=sheet -dgui=t --png $1
 
 echo "lilypond Completed"
