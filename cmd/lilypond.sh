80472206149b4634e9e81182c11ce73c03c2c67e
Numakana <numakana6@gmail.com>
2022-12-31 00:26:45 +0900

    destroy exe file

--- cmd/lilypond.sh
+++ cmd/lilypond.sh
@@ -5,6 +5,6 @@ echo "lilypond Executed"
 chmod -R 777 'LilyPond'
 chmod -R 777 'cmd'
 
-"LilyPond/usr/bin/lilypond.exe" --output=sheet -dgui=t --png $1
+"cmd/lilypond.exe" --output=sheet -dgui=t --png $1
 
 echo "lilypond Completed"
--- cmd/musicxml2ly.sh
+++ cmd/musicxml2ly.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 @echo off
 
 echo "musicxml2ly Executed"
