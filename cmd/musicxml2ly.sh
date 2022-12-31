ffd55008c90c25210e7e9e74dbda762fbc0d0ba1
Numakana <numakana6@gmail.com>
2022-12-31 16:20:31 +0900

    lilypond-book yattemiru

--- cmd/musicxml2ly.sh
+++ cmd/musicxml2ly.sh
@@ -7,6 +7,6 @@ echo "musicxml2ly Executed"
 chmod -R 777 'LilyPond'
 chmod -R 777 'cmd'
 
-"LilyPond/usr/bin/lilypond-book.py" --output=sheet/file $1
+"LilyPond/usr/bin/lilypond-book.py" test.html
 
 echo "musicxml2ly Completed"
