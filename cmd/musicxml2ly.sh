075f2fc12110009059290da958f1b81d5924cf37
Numakana <numakana6@gmail.com>
2022-12-31 16:23:12 +0900

    modified

--- cmd/musicxml2ly.sh
+++ cmd/musicxml2ly.sh
@@ -7,6 +7,6 @@ echo "musicxml2ly Executed"
 chmod -R 777 'LilyPond'
 chmod -R 777 'cmd'
 
-"LilyPond/usr/bin/lilypond-book.py" --output=test test.html
+"LilyPond/usr/bin/lilypond-book.py" --output==test test.html
 
 echo "musicxml2ly Completed"
