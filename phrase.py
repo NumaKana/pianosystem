94cd2107deaaa6221dba6418d1e8147e7774197a
NumaKana <82645804+NumaKana@users.noreply.github.com>
2023-04-12 18:41:54 +0900

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,7 +1,6 @@
 import re
 
 import makefile
-import search_phrase
 
 change_LightSteelBlue = """
 \override NoteHead.color = #(x11-color "LightSteelBlue")
@@ -33,8 +32,6 @@ def getphrase(m):
     i = 0
     start = []
     end = []
-    s_start, s_end = search_phrase.search()
-    print(s_end)
     while(i < len(data)): 
         if "\score {" in data[i]:
             break     
@@ -44,33 +41,21 @@ def getphrase(m):
             data.insert(i+1, change_LightSteelBlue)
             count = 1
             i += 2
-            s = 0
             while(count > 0):
-                print(s)
                 if "{" in data[i]: count += 1
                 if "}" in data[i]: count -= 1
-                s += len(re.findall('[a-g][is]*\'*,*\d+|\(|\)|\[|\]|-\.', data[i]))
-                if s >= s_start[m]:
-                    s -= len(re.findall('[a-g][is]*\'*,*\d+|\(|\)|\[|\]|-\.', data[i]))
-                    j = 0
-                    while(j < len(data[i])):
-                        if re.match('[a-g][is]*\'*,*\d+|\(|\)|\[|\]|-\.',data[i][j]): s += 1
-                        if s == s_start[m]:
-                            start.append([i, j])
-                        if s == s_end[m]:
-                            end.append([i, j])
-                        j+=1
-                if s == s_end[m]:
-                    end.append([i, j])
+                if "(" in data[i]: start.append(i)
+                if ")" in data[i]: end.append(i)
                 i+=1
         i+=1
     
-    print(end)
+    idx_start = list(data[start[m]]).index("(") -4
+    idx_end = list(data[end[m]]).index(")") + 2
+    if len(list(data[end[m]])) > idx_end:
+        if list(data[end[m]])[idx_end] == "]": idx_end += 2
 
-    idx_start = start[m][1]
-    idx_end = end[m][1]
-    data[end[m][0]] = data[end[m][0]][:idx_end] + ' ' + change_LightSteelBlue + ' ' + data[end[m][0]][idx_end:]
-    data[start[m][0]] = data[start[m][0]][:idx_start] + ' ' + change_Black + ' ' + data[start[m][0]][idx_start:]
+    data[end[m]] = data[end[m]][:idx_end] + ' ' + change_LightSteelBlue + ' ' + data[end[m]][idx_end:]
+    data[start[m]] = data[start[m]][:idx_start] + ' ' + change_Black + ' ' + data[start[m]][idx_start:]
 
     with open("phrase/file.ly", mode='w+', encoding="utf-8") as f:
         f.writelines(data)
